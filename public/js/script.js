6b267bc66aacbae35a3ecee2fdc6c9a80d2b95a2
amandugar <amanamandugar@gmail.com>
2021-01-13 17:45:49 +0530

    second

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,131 @@
+var cursor = {
+	delay: 8,
+	_x: 0,
+	_y: 0,
+	endX: (window.innerWidth / 2),
+	endY: (window.innerHeight / 2),
+	cursorVisible: true,
+	cursorEnlarged: false,
+	$dot: document.querySelector('.cursor-dot'),
+	$outline: document.querySelector('.cursor-dot-outline'),
+
+	init: function () {
+		// Set up element sizes
+		this.dotSize = this.$dot.offsetWidth;
+		this.outlineSize = this.$outline.offsetWidth;
+
+		this.setupEventListeners();
+		this.animateDotOutline();
+	},
+
+	//     updateCursor: function(e) {
+	//         var self = this;
+
+	//         console.log(e)
+
+	//         // Show the cursor
+	//         self.cursorVisible = true;
+	//         self.toggleCursorVisibility();
+
+	//         // Position the dot
+	//         self.endX = e.pageX;
+	//         self.endY = e.pageY;
+	//         self.$dot.style.top = self.endY + 'px';
+	//         self.$dot.style.left = self.endX + 'px';
+	//     },
+
+	setupEventListeners: function () {
+		var self = this;
+
+		// Anchor hovering
+		document.querySelectorAll('a').forEach(function (el) {
+			el.addEventListener('mouseover', function () {
+				self.cursorEnlarged = true;
+				self.toggleCursorSize();
+			});
+			el.addEventListener('mouseout', function () {
+				self.cursorEnlarged = false;
+				self.toggleCursorSize();
+			});
+		});
+
+		// Click events
+		document.addEventListener('mousedown', function () {
+			self.cursorEnlarged = true;
+			self.toggleCursorSize();
+		});
+		document.addEventListener('mouseup', function () {
+			self.cursorEnlarged = false;
+			self.toggleCursorSize();
+		});
+
+
+		document.addEventListener('mousemove', function (e) {
+			// Show the cursor
+			self.cursorVisible = true;
+			self.toggleCursorVisibility();
+
+			// Position the dot
+			self.endX = e.pageX;
+			self.endY = e.pageY;
+			self.$dot.style.top = self.endY + 'px';
+			self.$dot.style.left = self.endX + 'px';
+		});
+
+		// Hide/show cursor
+		document.addEventListener('mouseenter', function (e) {
+			self.cursorVisible = true;
+			self.toggleCursorVisibility();
+			self.$dot.style.opacity = 1;
+			self.$outline.style.opacity = 1;
+		});
+
+		document.addEventListener('mouseleave', function (e) {
+			self.cursorVisible = true;
+			self.toggleCursorVisibility();
+			self.$dot.style.opacity = 0;
+			self.$outline.style.opacity = 0;
+		});
+	},
+
+	animateDotOutline: function () {
+		var self = this;
+
+		self._x += (self.endX - self._x) / self.delay;
+		self._y += (self.endY - self._y) / self.delay;
+		self.$outline.style.top = self._y + 'px';
+		self.$outline.style.left = self._x + 'px';
+
+		requestAnimationFrame(this.animateDotOutline.bind(self));
+	},
+
+	toggleCursorSize: function () {
+		var self = this;
+
+		if (self.cursorEnlarged) {
+			self.$dot.style.transform = 'translate(-50%, -50%) scale(0.75)';
+			self.$outline.style.transform = 'translate(-50%, -50%) scale(1.5)';
+		} else {
+			self.$dot.style.transform = 'translate(-50%, -50%) scale(1)';
+			self.$outline.style.transform = 'translate(-50%, -50%) scale(1)';
+		}
+	},
+
+	toggleCursorVisibility: function () {
+		var self = this;
+
+		if (self.cursorVisible) {
+			self.$dot.style.opacity = 1;
+			self.$outline.style.opacity = 1;
+		} else {
+			self.$dot.style.opacity = 0;
+			self.$outline.style.opacity = 0;
+		}
+	}
+}
+
+cursor.init();
+
 $(window).scroll(function () {
 	if ($(this).scrollTop() > 80) {
 		document.getElementById("navbarTop").style.backgroundColor = "#FFFFFF";
@@ -114,33 +242,32 @@ var galleryThumbs = new Swiper('.gallery-thumbs', {
 	//   modifier: 1,
 	//   slideShadows : true,
 	// },
-	
+
 	coverflowEffect: {
-        rotate: 0,
-        stretch: 0,
-        depth: 50,
-        modifier: 6,
-        slideShadows : false,
-	  },
-	  
-  });
-  
-  
+		rotate: 0,
+		stretch: 0,
+		depth: 50,
+		modifier: 6,
+		slideShadows: false,
+	},
+
+});
+
+
 var galleryTop = new Swiper('.swiper-container.testimonial', {
 	speed: 400,
 	spaceBetween: 50,
 	autoplay: {
-	  delay: 3000,
-	  disableOnInteraction: false,
+		delay: 3000,
+		disableOnInteraction: false,
 	},
 	direction: 'vertical',
 	pagination: {
-	  clickable: true,
-	  el: '.swiper-pagination',
-	  type: 'bullets',
+		clickable: true,
+		el: '.swiper-pagination',
+		type: 'bullets',
 	},
 	thumbs: {
 		swiper: galleryThumbs
-	  }
-  });
-  
+	}
+});
