f443fbe468b5bf528fafe98e3e401c33d5c492ad
amandugar <amanamandugar@gmail.com>
2021-03-10 12:53:47 +0530

    yes

--- app.js
+++ app.js
@@ -2,20 +2,44 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const app = express();
 const ejs = require("ejs")
+const fs = require("fs")
 
 const port = 8080;
 
+
+
 app.use(bodyParser.urlencoded({
-    extended:true
+    extended: true
 }))
 
+let services;
 app.use(express.static("public"))
-app.set('view engine','ejs')
+app.set('view engine', 'ejs')
 
-app.get("/",function(req,res){
+fs.readFile('services.json', function (err, data) {
+    if (err) throw err;
+    services = JSON.parse(data);
+    console.log(services[0].knowMore[0].heading)
+})
+
+app.get("/", function (req, res) {
     res.render("home")
 })
 
-app.listen(port,function(req,res){
+app.get("/about", function (req, res) {
+    res.render("about")
+})
+
+app.get("/services", function (req, res) {
+    res.render("services",{
+        services:services
+    })
+})
+
+app.get("/blogs", function (req, res) {
+    res.render("blogs")
+})
+
+app.listen(port, function (req, res) {
     console.log(`Listening on port ${port}`)
 })
--- public/js/script.js
+++ public/js/script.js
@@ -17,23 +17,6 @@ var cursor = {
 		this.setupEventListeners();
 		this.animateDotOutline();
 	},
-
-	//     updateCursor: function(e) {
-	//         var self = this;
-
-	//         console.log(e)
-
-	//         // Show the cursor
-	//         self.cursorVisible = true;
-	//         self.toggleCursorVisibility();
-
-	//         // Position the dot
-	//         self.endX = e.pageX;
-	//         self.endY = e.pageY;
-	//         self.$dot.style.top = self.endY + 'px';
-	//         self.$dot.style.left = self.endX + 'px';
-	//     },
-
 	setupEventListeners: function () {
 		var self = this;
 
@@ -124,8 +107,6 @@ var cursor = {
 	}
 }
 
-cursor.init();
-
 $(window).scroll(function () {
 	if ($(this).scrollTop() > 80) {
 		document.getElementById("navbarTop").style.backgroundColor = "#FFFFFF";
@@ -134,140 +115,155 @@ $(window).scroll(function () {
 	}
 });
 
-$(document).ready(function () {
-	$("#tags").hide();
-	$('#myCanvas').tagcanvas({
-		textColour: '#444;',
-		textFont: `'Lato', sans-serif"`,
-		outlineColour: 'rgba(0,0,0,0)',
-		bgOutline: null,
-		freezeDecel: true,
-		reverse: true,
-		depth: .6,
-		initial: [0.1, 0.1],
-		maxSpeed: 0.1,
-		dragControl: true,
-		minSpeed: 0.001
-	}, 'tags')
-});
+cursor.init();
 
-$('.single-item-rtl').slick({
-	dots: true,
-	infinite: true,
-	speed: 3000,
-	autoplay: true,
-	autoplaySpeed: 3000,
-	cssease: 'ease-in',
-	slidesToShow: 1,
-	pauseOnHover: false,
-	pauseOnFocus: false,
-	slidesToScroll: 1,
-	dotsc: true,
-	prevArrow: `<i class="bi bi-chevron-left a-left control-c prev slick-prev"></i>`,
-	nextArrow: `<i class="bi bi-chevron-right a-right control-c next slick-next"></i>`
-});
-let canva = document.getElementById("myCanvas");
-setInterval(function () {
-	if (screen.width >= 960) {
-		canva.width = 400;
-		canva.height = 400;
-	} else if (screen.width < 960 && screen.width >= 768) {
-		canva.width = 400;
-		canva.height = 400;
-	} else if (screen.width < 768 && screen.width >= 350) {
-		canva.width = 350;
-		canva.height = 350;
-	} else {
-		canva.width = 270;
-		canva.height = 270;
+if (window.location.pathname === "/") {
+
+
+	// $(document).ready(function () {
+	// 	$("#tags").hide();
+	// 	$('#myCanvas').tagcanvas({
+	// 		textColour: '#444;',
+	// 		textFont: `'Poppins', sans-serif`,
+	// 		outlineColour: 'rgba(0,0,0,0)',
+	// 		bgOutline: null,
+	// 		freezeDecel: true,
+	// 		reverse: true,
+	// 		depth: .6,
+	// 		initial: [0.1, 0.1],
+	// 		maxSpeed: 0.1,
+	// 		dragControl: true,
+	// 		minSpeed: 0.001
+	// 	}, 'tags')
+	// });
+
+	$('.single-item-rtl').slick({
+		dots: true,
+		infinite: true,
+		speed: 3000,
+		autoplay: true,
+		autoplaySpeed: 3000,
+		cssease: 'ease-in',
+		slidesToShow: 1,
+		pauseOnHover: false,
+		pauseOnFocus: false,
+		slidesToScroll: 1,
+		dotsc: true,
+		prevArrow: `<i class="bi bi-chevron-left a-left control-c prev slick-prev"></i>`,
+		nextArrow: `<i class="bi bi-chevron-right a-right control-c next slick-next"></i>`
+	});
+	if (window.location.pathname === "/") {
+		let canva = document.getElementById("myCanvas");
+		setInterval(function () {
+			if (screen.width >= 960) {
+				canva.width = 400;
+				canva.height = 400;
+			} else if (screen.width < 960 && screen.width >= 768) {
+				canva.width = 400;
+				canva.height = 400;
+			} else if (screen.width < 768 && screen.width >= 350) {
+				canva.width = 350;
+				canva.height = 350;
+			} else {
+				canva.width = 270;
+				canva.height = 270;
+			}
+		}, 1000)
 	}
-}, 1000)
 
 
+	// Params
+	var sliderSelector = '.swipe1',
+		options = {
+			init: false,
+			loop: true,
+			speed: 800,
+			slidesPerView: 1, // or 'auto'
+			// spaceBetween: 10,
+			centeredSlides: true,
+			effect: 'coverflow', // 'cube', 'fade', 'coverflow',
+			coverflowEffect: {
+				rotate: 50,
+				stretch: 0,
+				depth: 100,
+				modifier: 1,
+				slideShadows: true,
+			},
+			grabCursor: true,
+			parallax: true,
+			pagination: {
+				el: '.swiper-pagination',
+				clickable: true,
+			},
+			navigation: {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev',
+			},
+			breakpoints: {
+				1025: {
+					slidesPerView: 3,
+					spaceBetween: 0
+				}
+			},
+			// Events
+			on: {
+				imagesReady: function () {
+					this.el.classList.remove('loading');
+				}
+			}
+		};
+	var mySwiper = new Swiper(sliderSelector, options);
 
-// Params
-var sliderSelector = '.swipe1',
-	options = {
-		init: false,
-		loop: true,
-		speed: 800,
-		slidesPerView: 1, // or 'auto'
-		// spaceBetween: 10,
-		centeredSlides: true,
-		effect: 'coverflow', // 'cube', 'fade', 'coverflow',
-		coverflowEffect: {
-			rotate: 50, // Slide rotate in degrees
-			stretch: 0, // Stretch space between slides (in px)
-			depth: 100, // Depth offset in px (slides translate in Z axis)
-			modifier: 1, // Effect multipler
-			slideShadows: true, // Enables slides shadows
-		},
+
+	mySwiper.init();
+
+	var galleryThumbs = new Swiper('.gallery-thumbs', {
+		effect: 'coverflow',
 		grabCursor: true,
-		parallax: true,
+		centeredSlides: true,
+		slidesPerView: '2',
+		coverflowEffect: {
+			rotate: 0,
+			stretch: 0,
+			depth: 50,
+			modifier: 6,
+			slideShadows: false,
+		},
+
+	});
+
+
+	var galleryTop = new Swiper('.swiper-container.testimonial', {
+		speed: 400,
+		spaceBetween: 50,
+		autoplay: {
+			delay: 3000,
+			disableOnInteraction: false,
+		},
+		direction: 'vertical',
 		pagination: {
-			el: '.swiper-pagination',
 			clickable: true,
+			el: '.swiper-pagination',
+			type: 'bullets',
 		},
-		navigation: {
-			nextEl: '.swiper-button-next',
-			prevEl: '.swiper-button-prev',
-		},
-		breakpoints: {
-			1025: {
-				slidesPerView: 3,
-				spaceBetween: 0
-			}
-		},
-		// Events
-		on: {
-			imagesReady: function () {
-				this.el.classList.remove('loading');
-			}
+		thumbs: {
+			swiper: galleryThumbs
 		}
-	};
-var mySwiper = new Swiper(sliderSelector, options);
+	});
+}
+if (window.location.pathname === "/about") {
+	var myIndex = 0;
+	carousel();
 
-// Initialize slider
-mySwiper.init();
-
-var galleryThumbs = new Swiper('.gallery-thumbs', {
-	effect: 'coverflow',
-	grabCursor: true,
-	centeredSlides: true,
-	slidesPerView: '2',
-	// coverflowEffect: {
-	//   rotate: 50,
-	//   stretch: 0,
-	//   depth: 100,
-	//   modifier: 1,
-	//   slideShadows : true,
-	// },
-
-	coverflowEffect: {
-		rotate: 0,
-		stretch: 0,
-		depth: 50,
-		modifier: 6,
-		slideShadows: false,
-	},
-
-});
-
-
-var galleryTop = new Swiper('.swiper-container.testimonial', {
-	speed: 400,
-	spaceBetween: 50,
-	autoplay: {
-		delay: 3000,
-		disableOnInteraction: false,
-	},
-	direction: 'vertical',
-	pagination: {
-		clickable: true,
-		el: '.swiper-pagination',
-		type: 'bullets',
-	},
-	thumbs: {
-		swiper: galleryThumbs
+	function carousel() {
+		var i;
+		var x = document.getElementsByClassName("mySlides");
+		for (i = 0; i < x.length; i++) {
+			x[i].style.display = "none";
+		}
+		myIndex++;
+		if (myIndex > x.length) { myIndex = 1 }
+		x[myIndex - 1].style.display = "block";
+		setTimeout(carousel, 2000); // Change image every 2 seconds
 	}
-});
+}
