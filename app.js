2082fc66887e72e55d7b647e70cf4277dc20a112
amandugar <amanamandugar@gmail.com>
2021-03-12 16:27:18 +0530

    dashboard

--- app.js
+++ app.js
@@ -5,13 +5,16 @@ const mongoose = require("mongoose")
 const session = require('express-session');
 const passport = require("passport");
 const fs = require("fs");
+const fileUpload = require("express-fileupload")
 const passportLocalMongoose = require("passport-local-mongoose");
 const app = express();
 
 const port = 8080;
 
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(express.static("public"));
 
 app.use(session({
@@ -20,25 +23,45 @@ app.use(session({
     saveUninitialized: false
 }));
 
+app.use(fileUpload());
+
 let usernameAuthenticated;
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-mongoose.connect("mongodb+srv://njoyurage:njoyurage@cluster0.pjnmd.mongodb.net/nJoyUrAge?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb+srv://njoyurage:njoyurage@cluster0.pjnmd.mongodb.net/nJoyUrAge?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 mongoose.set("useCreateIndex", true);
 
 const userSchema = new mongoose.Schema({
     name: String,
-    email: { type: String, unique: true },
-    username: { type: String, unique: true },
+    email: {
+        type: String,
+        unique: true
+    },
+    username: {
+        type: String,
+        unique: true
+    },
     password: String,
 });
+
+const postSchema = new mongoose.Schema({
+    author: String,
+    title: String,
+    subTitle: String,
+    time: String,
+    content: String,
+    image: String
+})
 
 userSchema.plugin(passportLocalMongoose);
 
 const User = new mongoose.model("User", userSchema);
-
+const Post = new mongoose.model("Post", postSchema)
 
 passport.use(User.createStrategy());
 
@@ -116,7 +139,11 @@ app.get("/signup", function (req, res) {
 
 app.post("/signup", function (req, res) {
 
-    User.register({ email: req.body.email, username: req.body.username, name: req.body.name }, req.body.password, function (err, user) {
+    User.register({
+        email: req.body.email,
+        username: req.body.username,
+        name: req.body.name
+    }, req.body.password, function (err, user) {
         if (err) {
             console.log(err);
             res.redirect("/signup");
@@ -126,6 +153,67 @@ app.post("/signup", function (req, res) {
     });
 
 });
+
+app.get("/admin/:username/createBlog", function (req, res) {
+    if (req.isAuthenticated() && usernameAuthenticated == req.params.username) {
+        res.render("create", {
+            username: req.params.username
+        })
+    } else {
+        res.redirect("/blog")
+    }
+})
+
+let image;
+
+app.post("/admin/:username/createBlog", function (req, res) {
+    if (req.isAuthenticated() && usernameAuthenticated == req.params.username) {
+        let author = "NJoyUrAge";
+        let title = req.body.title;
+        let content = req.body.content;
+        let subTitle = req.body.subTitle;
+        let time = Date(Date.now());
+
+        console.log(req.body)
+        if (!req.files || Object.keys(req.files).length === 0) {
+            res.status(400).send('No files were uploaded.');
+            return;
+        }
+
+        console.log('req.files >>>', req.files); // eslint-disable-line
+
+        image = req.files.image;
+
+        uploadPath = __dirname + '/public/images/uploads/' + image.name;
+
+        image.mv(uploadPath, function (err) {
+            if (err) {
+                return res.status(500).send(err);
+            }
+
+            const newPost = new Post({
+                author: author,
+                title: title,
+                subTitle: subTitle,
+                content: content,
+                time: time,
+                image: image.name
+            })
+            newPost.save(function (err, post) {
+                if (err) {
+                    console.log(err)
+                    res.render("404")
+                } else {
+                    res.redirect("/admin/" + usernameAuthenticated)
+                }
+            })
+        });
+
+
+    } else {
+        res.redirect("/blog")
+    }
+})
 
 app.get("/logout", function (req, res) {
     req.logout();
